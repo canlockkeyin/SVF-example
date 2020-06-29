d5b97f5666b790ca01da7b387702408732b604df
Yannick Loeck <yannick.loeck@gmail.com>
2020-06-29 15:44:26 +0200

    update svf-ex: output values and their callpaths

--- src/svf-ex.cpp
+++ src/svf-ex.cpp
@@ -1,182 +1,123 @@
-//===- svf-ex.cpp -- A driver example of SVF-------------------------------------//
-//
-//                     SVF: Static Value-Flow Analysis
-//
-// Copyright (C) <2013->  <Yulei Sui>
-//
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//===-----------------------------------------------------------------------===//
-
-/*
- // A driver program of SVF including usages of SVF APIs
- //
- // Author: Yulei Sui,
- */
-
 #include "SVF-FE/LLVMUtil.h"
 #include "Graphs/SVFG.h"
 #include "WPA/Andersen.h"
-#include "SABER/LeakChecker.h"
 
+SVFModule* svfModule;
+PTACallGraph* callgraph;
 
-using namespace llvm;
-using namespace std;
-
-static llvm::cl::opt<std::string> InputFilename(cl::Positional,
-        llvm::cl::desc("<input bitcode>"), llvm::cl::init("-"));
-
-/*!
- * An example to query alias results of two LLVM values
- */
-AliasResult aliasQuery(PointerAnalysis* pta, Value* v1, Value* v2){
-	return pta->alias(v1,v2);
+std::string printVal(const Value* v) {
+    std::string val_str;
+    raw_string_ostream rso(val_str);
+    v->print(rso);
+    return rso.str();
 }
 
-/*!
- * An example to print points-to set of an LLVM value
- */
-std::string printPts(PointerAnalysis* pta, Value* val){
-
-    std::string str;
-    raw_string_ostream rawstr(str);
-
-    NodeID pNodeId = pta->getPAG()->getValueNode(val);
-    NodeBS& pts = pta->getPts(pNodeId);
-    for (NodeBS::iterator ii = pts.begin(), ie = pts.end();
-            ii != ie; ii++) {
-        rawstr << " " << *ii << " ";
-        PAGNode* targetObj = pta->getPAG()->getPAGNode(*ii);
-        if(targetObj->hasValue()){
-            rawstr << "(" <<*targetObj->getValue() << ")\t ";
+void getCallPaths(const Function* f, std::vector<std::vector<const Instruction*>>& paths, std::vector<const Instruction*>& curPath) {
+    if (const SVFFunction* sf = svfModule->getSVFFunction(f)) {
+        PTACallGraphNode* cgn = callgraph->getCallGraphNode(sf);
+        /* check if further path exists */
+        if (cgn->hasIncomingEdge()) {
+            /* incoming edges of this node */
+            for (auto edgit = cgn->InEdgeBegin(); edgit != cgn->InEdgeEnd(); ++edgit) {
+                PTACallGraphEdge* edg = *edgit;
+                PTACallGraphEdge::CallInstSet cis = edg->getDirectCalls();
+                for (const CallBlockNode* cbn : cis) {
+                    const Function* cf = cbn->getCallSite()->getFunction();
+                    //const Instruction* ci = cbn->getCallSite()->getInstruction();
+                    const Instruction* ci = cbn->getCallSite();
+                    curPath.push_back(ci);
+                    getCallPaths(cf, paths, curPath);
+                    /* when the recursion above finishes, we have reached a root node
+                     * therefore we go back (down) one node and check its other parents
+                     */
+                    curPath.pop_back();
+                }
+            }
+        }
+        /* end of this callpath reached */
+        else {
+            paths.push_back(curPath);
         }
     }
-
-    return rawstr.str();
-
 }
 
-
-/*!
- * An example to query/collect all successor nodes from a ICFGNode (iNode) along control-flow graph (ICFG)
- */
-void traverseOnICFG(ICFG* icfg, const Instruction* inst){
-	ICFGNode* iNode = icfg->getBlockICFGNode(inst);
-	FIFOWorkList<const ICFGNode*> worklist;
-	std::set<const ICFGNode*> visited;
-	worklist.push(iNode);
-
-	/// Traverse along VFG
-	while (!worklist.empty()) {
-		const ICFGNode* vNode = worklist.pop();
-		for (ICFGNode::const_iterator it = iNode->OutEdgeBegin(), eit =
-				iNode->OutEdgeEnd(); it != eit; ++it) {
-			ICFGEdge* edge = *it;
-			ICFGNode* succNode = edge->getDstNode();
-			if (visited.find(succNode) == visited.end()) {
-				visited.insert(succNode);
-				worklist.push(succNode);
-			}
-		}
-	}
-}
-
-/*!
- * An example to query/collect all the uses of a definition of a value along value-flow graph (VFG)
- */
-void traverseOnVFG(const SVFG* vfg, Value* val){
+void collectUsesOnVFG(const SVFG& vfg, const Value& val){
 	PAG* pag = PAG::getPAG();
 
-    PAGNode* pNode = pag->getPAGNode(pag->getValueNode(val));
-    const VFGNode* vNode = vfg->getDefSVFGNode(pNode);
+    PAGNode* pNode = pag->getPAGNode(pag->getValueNode(&val));
+    const VFGNode* vNode = vfg.getDefSVFGNode(pNode);
     FIFOWorkList<const VFGNode*> worklist;
     std::set<const VFGNode*> visited;
     worklist.push(vNode);
 
-	/// Traverse along VFG
-	while (!worklist.empty()) {
-		const VFGNode* vNode = worklist.pop();
-		for (VFGNode::const_iterator it = vNode->OutEdgeBegin(), eit =
-				vNode->OutEdgeEnd(); it != eit; ++it) {
-			VFGEdge* edge = *it;
-			VFGNode* succNode = edge->getDstNode();
-			if (visited.find(succNode) == visited.end()) {
-				visited.insert(succNode);
-				worklist.push(succNode);
-			}
-		}
-	}
-
+    /// Traverse along VFG
+    while(!worklist.empty()){
+        const VFGNode* vNode = worklist.pop();
+        visited.insert(vNode);
+        for(VFGNode::const_iterator it = vNode->OutEdgeBegin(); it != vNode->OutEdgeEnd(); ++it) {
+            if(visited.find((*it)->getDstNode())==visited.end()){
+                worklist.push((*it)->getDstNode());
+            }
+        }
+    }
     /// Collect all LLVM Values
     for(std::set<const VFGNode*>::const_iterator it = visited.begin(), eit = visited.end(); it!=eit; ++it){
     	const VFGNode* node = *it;
     	/// can only query VFGNode involving top-level pointers (starting with % or @ in LLVM IR)
-    	/// PAGNode* pNode = vfg->getLHSTopLevPtr(node);
-    	/// Value* val = pNode->getValue();
+        //std::cout << "node kind: " << node->getNodeKind() << std::endl;
+        if (node->getNodeKind() == 3)
+            continue;
+        const PAGNode* pNode = vfg.getLHSTopLevPtr(node);
+        const Value* val = pNode->getValue();
+
+        std::vector<const Instruction*> cp;
+        std::vector<std::vector<const Instruction*>> ps;
+        if (const Function* f = pNode->getFunction()) {
+            getCallPaths(f, ps, cp);
+        }
+        //std::cout << "Value: " << printVal(val) << std::endl;
+        int i = 0;
+        for (auto p : ps) {
+            if (p.empty())
+                continue;
+            std::cout << "path " << i << ": \n        |--";
+            for (auto i : p) {
+                std::cout << printVal((Value*)i) << "\n        ---";
+            }
+            std::cout << (*(p.end() -1))->getFunction()->getName().str() << "|" << std::endl;
+            i++;
+        }
     }
 }
 
-int main(int argc, char ** argv) {
 
+int main(int argc, char ** argv) {
     int arg_num = 0;
     char **arg_value = new char*[argc];
     std::vector<std::string> moduleNameVec;
     SVFUtil::processArguments(argc, argv, arg_num, arg_value, moduleNameVec);
-    cl::ParseCommandLineOptions(arg_num, arg_value,
-                                "Whole Program Points-to Analysis\n");
 
-    SVFModule* svfModule = LLVMModuleSet::getLLVMModuleSet()->buildSVFModule(moduleNameVec);
-
+    svfModule = LLVMModuleSet::getLLVMModuleSet()->buildSVFModule(moduleNameVec);
     Andersen* ander = AndersenWaveDiff::createAndersenWaveDiff(svfModule);
 
-    /// Query aliases
-    /// aliasQuery(ander,value1,value2);
-
-    /// Print points-to information
-    /// printPts(ander, value1);
-
-    /// Program Assignment Graph (PAG)
     PAG* pag = ander->getPAG();
-    pag->dump("pag");
-
-    /// Call Graph
-    PTACallGraph* callgraph = ander->getPTACallGraph();
-    callgraph->dump("callgraph");
-
-    /// ICFG
-    ICFG* icfg = pag->getICFG();
-    icfg->dump("icfg");
-
-    /// Value-Flow Graph (VFG)
-    VFG* vfg = new VFG(callgraph);
-    vfg->dump("vfg");
-
-    /// Sparse value-flow graph (SVFG)
+    callgraph = ander->getPTACallGraph();
+    callgraph->dump("cg");
     SVFGBuilder svfBuilder;
-    SVFG* svfg = svfBuilder.buildFullSVFGWithoutOPT(ander);
-    svfg->dump("svfg");
+    SVFG* svfg = svfBuilder.buildFullSVFG(ander);
 
-    /// Collect uses of an LLVM Value
-    /// traverseOnVFG(svfg, value);
-
-    /// Collect all successor nodes on ICFG
-    /// traverseOnICFG(icfg, value);
-
-    /// Perform memory leak detection
-    LeakChecker* saber = new LeakChecker();  // if no checker is specified, we use leak checker as the default one.
-    saber->runOnModule(svfModule);
-    return 0;
+    for (auto it = svfModule->global_begin(); it != svfModule->global_end(); ++it) {
+        llvm::Value* v = *it;
+        std::cout << "Value: " << printVal(v) << std::endl;
+        collectUsesOnVFG(*svfg, *v);
+    }
+    /*
+    for (auto it = svfModule->llvmFunBegin(); it != svfModule->llvmFunEnd(); ++it) {
+        llvm::Function* f = *it;
+        std::cout << "operands: " << f->getNumOperands() << std::endl;
+        //std::cout << "Function: " << f->getName().str() << std::endl;
+        //std::cout << (*it)->getName().str() << std::endl;
+        collectUsesOnVFG(*svfg, *f);
+    }
+    */
 }
-
